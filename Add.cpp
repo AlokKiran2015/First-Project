282c4f5c794cf5b52bfec29ba6adeca2d0e81158
AlokKiran2015 <2015csb1006@iitrpr.ac.in>
2017-04-20 16:02:34 +0530

    Create Add.cpp

---
+++ Add.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int add(int a,int b){
+
+return a+b;
+}
+
+int main(){
+int a,b;
+cout<<"enter numbers"<<endl;
+cin>>a>>b;
+cout<<"Sum of the two numbers is: "<<add(a,b)<<endl;
+
+}
